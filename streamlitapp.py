b74eed376b5db5ed206bdd08274c7fc947d08cba
Sai Morcom <129870080+smorcom23@users.noreply.github.com>
2023-07-24 11:40:20 -0400

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,15 +1,15 @@
+import streamlit as st
 import snowflake.connector
 import os
-import streamlit as st
 
 def execute_procedure(db_name, role_name):
-    # Read Snowflake connection information from GitHub secrets
+    # Read Snowflake connection information from the environment variables
     snowflake_config = {
-        "user": os.environ["SNOWFLAKE_USER"],
-        "password": os.environ["SNOWFLAKE_PASSWORD"],
-        "account": os.environ["SNOWFLAKE_ACCOUNT"],
-        "database": os.environ["SNOWFLAKE_DATABASE"],
-        "schema": os.environ["SNOWFLAKE_SCHEMA"]
+        "user": os.environ.get("SNOWFLAKE_USER"),
+        "password": os.environ.get("SNOWFLAKE_PASSWORD"),
+        "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
+        "database": os.environ.get("SNOWFLAKE_DATABASE"),
+        "schema": os.environ.get("SNOWFLAKE_SCHEMA")
     }
 
     # Create a Snowflake connection
