dfb224b53a3d47d500cd6c034e9de41a4738d9e4
Sai Morcom <129870080+smorcom23@users.noreply.github.com>
2023-07-25 13:04:23 -0400

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import snowflake.connector
 
+def check_role_existence(conn, role_name):
+    cursor = conn.cursor()
+    try:
+        # Check if the role exists with securityadmin role
+        cursor.execute(f"USE ROLE securityadmin; SHOW ROLES LIKE '{role_name}'")
+        role_exists = len(cursor.fetchall()) > 0
+        return role_exists
+    finally:
+        cursor.close()
+
 def execute_procedure(db_name, role_name):
     # Read Snowflake connection information from Streamlit secrets
     snowflake_config = st.secrets["snowflake"]
@@ -10,14 +20,26 @@ def execute_procedure(db_name, role_name):
         user=snowflake_config["snowflake_user"],
         password=snowflake_config["snowflake_password"],
         account=snowflake_config["snowflake_account"],
+        warehouse=snowflake_config["snowflake_warehouse"],
         database=snowflake_config["snowflake_database"],
         schema=snowflake_config["snowflake_schema"]
     )
+
+    # Check if the role exists with securityadmin role
+    role_exists = check_role_existence(conn, role_name)
+
+    if not role_exists:
+        st.error(f"The role '{role_name}' does not exist.")
+        conn.close()
+        return
 
     # Create a Snowflake cursor
     cursor = conn.cursor()
 
     try:
+        # Switch to sysadmin role
+        cursor.execute("USE ROLE sysadmin")
+
         # Call the procedure with provided parameters using array_construct
         cursor.execute(f"CALL ag_admin_db.public.create_lsra_db_crud(array_construct('{db_name}'), array_construct('{role_name}'))")
         st.success(f"Procedure executed successfully with Database: {db_name}, Role: {role_name}")
